04ae33760f64d08eb869a0e28de6e745e229b50d
HaeDalHwang <svvwac15@naver.com>
2021-09-13 11:45:49 +0900

    login8

--- login.php
+++ login.php
@@ -26,14 +26,14 @@ if($db){
 		//로그인 성공
 		echo '<script>';
 		echo 'console.log("Login Success");';
-		echo "location.replace('list.php');"
+		echo "location.replace('list.html');";
      	echo '</script>';
 	}else{
 		// 로그인 정보틀림
 		echo '<script>';
 		echo 'console.log("Login Error");';
 		echo 'alert("Login Error! 비밀번호나 아이디를 확인하세요!");';
-		echo "location.replace('index.php');"
+		echo "location.replace('index.html');";
 		echo '</script>';	
 	}
 }
@@ -42,7 +42,7 @@ else{
     echo '<script>';
     echo 'console.log("DB Connection Error");';
     echo 'alert("DB Connection Error!");';
-    echo "location.replace('index.php');"
+    echo "location.replace('index.html');";
     echo '</script>';
 }
 
