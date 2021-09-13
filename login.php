9fe552b42307c7aad5f2d8c180fef4c2fa7b68a1
HaeDalHwang <svvwac15@naver.com>
2021-09-13 10:07:25 +0900

    login4

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
-echo "<script>alert(\"TEST!\");</script>";
-
+echo '<script>';
+echo 'console.log("TEST")';
+echo '</script>';
 // Mariadb Connect TEST
 $host = "211.183.3.100";
 $id = "root";
@@ -20,18 +21,21 @@ if($db){
 	$result = mysqli_query($db,$sql);
 	if(mysqli_num_rows($result)==1){
 		//로그인 성공
-		echo "<script>alert(\"Login Success!\");</script>";
-        echo("<script>location.replace('list.html');</script>"); 
+		echo '<script>';
+		echo 'console.log("Login Success")';
+        echo '</script>';
 	}else{
 		// 로그인 정보틀림
-		echo "<script>alert(\"Login Error!\");</script>";
-		echo("<script>location.replace('index.html');</script>"); 	
+		echo '<script>';
+		echo 'console.log("Login Error")';
+		echo '</script>';	
 	}
 }
 else{
     // 실패부분
-    echo "<script>alert(\"DB connection Error!\");</script>";
-	 echo("<script>location.replace('index.html');</script>");
+    echo '<script>';
+    echo 'console.log("DB Connection Error")';
+    echo '</script>';
 }
 
 
