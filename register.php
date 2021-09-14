3321476637764f8272b04476a4b84c651e0a17fe
HaeDalHwang <svvwac15@naver.com>
2021-09-14 12:10:26 +0900

    error debuging

--- register.php
+++ register.php
@@ -9,13 +9,15 @@ $db = new mysqli($host, $id, $passwd, $dbname);
 
 if($db){
     $insert_id = $_POST['name'];
-    $insert_pwd = $_POST['password1'];
+    $insert_pwd = $_POST['userpwd'];
+    $insert_pwd2 = $_POST['userpwd2'];
     $insert_email = $POST['email'];
     $hash = password_hash($insert_pwd, PASSWORD_DEFAULT);
     $sql = "insert into member values ('$insert_id','$hash','$insert_email')";
     if(mysqli_query($db,$sql)){
         echo '<script>';
-        echo 'alert("Success register");';
+        echo 'alert("Success register'.$insert_pwd. '  '.$insert_pwd2.'");';
+
         echo "location.replace('index.html');";
         echo '</script>';
     }else{
@@ -30,6 +32,5 @@ if($db){
     echo "location.replace('index.html');";
     echo '</script>';
 }
-
 
 ?>
